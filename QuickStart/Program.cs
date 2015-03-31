7c3d02d84f794f40bdcfdfe96e121ea0cf48f1e5
Tsccai <ohyeahsohot@163.com>
2015-03-31 22:51:59 +0800

    delete files

--- QuickStart/Program.cs
+++ QuickStart/Program.cs
@@ -11,9 +11,9 @@ namespace QuickStart
     {
         static void Main(string[] args)
         {
-            string captureFilename = @"..\..\..\Test\CapturedFiles\20140813-150920_0005ED9B-50+60_MMS.pcap";
+            //string captureFilename = @"..\..\..\Test\CapturedFiles\20140813-150920_0005ED9B-50+60_MMS.pcap";
             // string captureFilename = @"..\..\..\Test\CapturedFiles\20140725-210910_00036E3E-20+20_RcdD05.pcap";
-           // string captureFilename = @"..\..\..\Test\CapturedFiles\20140826-113450-10+20_RcdD05_.pcap";
+            string captureFilename = @"..\..\..\Test\CapturedFiles\20140826-113450-10+20_RcdD05_.pcap";
             ResolveDevice dev = new ResolveDevice(captureFilename);
             DateTime start = DateTime.Now;
             dev.Open();
